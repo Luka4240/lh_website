b582dd15943fe156d8469445bc7d96beb4f3c958
Luke Howard <luke@lukehoward.me.uk>
2013-08-02 16:50:51 +0100

    1st Draft
    First run of site

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<!-- ======== Basic Page Needs ========= -->    
+<meta charset="utf-8">
+<title>Luke Howard // Designer, Developer and Geek</title>
+
+<!-- ======== Responsive Page Needs ========= -->    
+    
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!-- ======== Mooi Layout, and Responsive Needs ========= -->
+    
+<!-- HTML5Shiv.js & Respond.js -->
+<!--[if lte IE 8]>
+<script type="text/javscript" src="jquery/html5shiv.js"></script>
+<script type="text/javscript" src="jquery/respond.js"></script>
+<![endif]-->
+
+<!-- ======== CSS StyleSheets ========= -->    
+<link rel="stylesheet" href="//f.fontdeck.com/s/css/hHe2eeOPSef/0iNrBlOdAIYMato/lukehoward.me.uk/35410.css" type="text/css" />
+<link rel="stylesheet" href="//f.fontdeck.com/s/css/hHe2eeOPSef/0iNrBlOdAIYMato/www.lukehoward.me.uk/35410.css" type="text/css" />
+<link href="stylesheets/style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+<!-- ======== Mooi Layout, Delete everything below and create your own site! :-) ========= -->
+
+<header class="full-row page-header">
+    <div class="row">
+        <div class="sixteen columns">
+            <nav>
+                <ul class="mooi-menu-horizontal  mooi-menu-right">
+                    <li><a href="#">Home</a></li>    
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Work</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>        
+</header>    
+<div class="row">
+    <div class="sixteen columns">
+    <h3>Luke Howard</h3>
+        <p>Taking your website from idea to reality.</p>
+    </div>    
+    
+</div>
+<div class="row">
+    <div class="one-third column">
+        <h2>Developer</h2>
+        <p>Focusing mainly on HTML5 and CSS3 but also working with jQuery, PHP and MySQL to fully develop any site, as well as the ability to work with Concrete5 and WordPress.</p>    
+        
+    </div>
+    <div class="one-third column">
+    <p>A Funky Image?</p>
+    </div>
+    
+    <div class="one-third column">
+        <h2>Designer</h2>
+        <p>As well as being a developer, the ability to provide a website from start to finish, creating the concepts and wireframe, through to ensuring it is fully functioning.</p>
+    
+    </div>    
+    
+</div>
+
+<footer class="row">
+
+    <div class="four columns"><p>&copy; Copyright 2013. Luke Howard.</p></div>
+    <div class="four columns"></div>
+    <div class="four columns"></div>
+    <div class="four columns"></div>
+    
+</footer>    
+    
+
+</body>
+</html>
